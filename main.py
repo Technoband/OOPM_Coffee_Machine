38d116b052fae19efa64215cb16a38d977aeac56
Yashraj Zope <yashajzope143@gmail.com>
2023-10-05 19:06:51 +0530

    update main file

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # import turtle
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+table.field_names = ["Pokemon Name", "Type"]
+table.add_row(["Pikachu", "Electric"])
+table.add_row(["Squirtle", "Water"])
+table.add_row(["Charmender", "Fire"])
+
+print(table)
