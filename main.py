73308f52658540a94fad02428d16d12100817487
Yashraj Zope <yashajzope143@gmail.com>
2023-10-05 18:49:46 +0530

    add main file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import turtle
+# import turtle
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
 
-timmy = turtle.Turtle()
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
